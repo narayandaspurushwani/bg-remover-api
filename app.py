1aa7050f64054997003791b04c66ad639ec1a63f
narayandaspurushwani <purushwaninarayandas@gmail.com>
2025-09-14 04:45:29 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from gradio_client import Client
+import nest_asyncio
+import uvicorn
+
+nest_asyncio.apply()
+
+app = FastAPI()
+client = Client("NDPDevloper/RMBG")
+
+@app.post("/remove-bg/")
+async def remove_bg(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    result = client.predict([image_bytes], api_name="/predict")
+    return JSONResponse(content={"processed_image": result[0]})
