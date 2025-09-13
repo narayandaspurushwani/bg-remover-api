dbdece53b529b48e19e19ea585f12fb658e83011
narayandaspurushwani <purushwaninarayandas@gmail.com>
2025-09-14 02:58:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+from rembg import remove
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    input_bytes = await file.read()
+    output_bytes = remove(input_bytes)
+    return Response(content=output_bytes, media_type="image/png")
