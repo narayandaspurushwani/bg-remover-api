b4d456da40ad4f4365672960a2c483dbb8e72a03
narayandaspurushwani <purushwaninarayandas@gmail.com>
2025-09-14 05:31:34 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from gradio_client import Client
 import nest_asyncio
+import uvicorn
 
 nest_asyncio.apply()
 app = FastAPI()
@@ -10,6 +11,12 @@ client = Client("NDPDevloper/RMBG")
 
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
-    image_bytes = await file.read()
-    result = client.predict([image_bytes], api_name="/predict")
-    return JSONResponse(content={"processed_image": result[0]})
+    try:
+        image_bytes = await file.read()
+        result = client.predict([image_bytes], api_name="/predict")
+        return JSONResponse(content={"processed_image": result[0]})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
