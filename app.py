949749d3d0654d61993cf61e8a689fd87972596a
narayandaspurushwani <purushwaninarayandas@gmail.com>
2025-09-14 14:30:48 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import base64
 
 app = FastAPI()
 
+# This is the new endpoint for the root URL
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the BG Remover API. Use the /remove-bg/ endpoint to remove backgrounds from images. You can find the documentation at /docs"}
+
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
     image_bytes = await file.read()
